b232c97b9ad5aa0c4be71e048c96e99c5e62ebf0
python019 <tommyjumayev@gmail.com>
2022-04-30 13:28:42 +0500

    3-dars

--- lanapp/admin.py
+++ lanapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Malumot
 # Register your models here.
+
+@admin.register(Malumot)
+class MalumotAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('nomi',)}
--- lanapp/models.py
+++ lanapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Malumot(models.Model):
+    nomi = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.nomi
