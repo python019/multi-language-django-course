f26ce4c3d9bd75fe5fd51cdf15005e21ac2828b7
python019 <tommyjumayev@gmail.com>
2022-05-06 11:19:46 +0500

    8-dars

--- multilan/urls.py
+++ multilan/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+] + i18n_patterns(
     path('', include('lanapp.urls'))
-]
+)
